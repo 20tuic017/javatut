ff456ab15ae60d61cfd5dea2f834c09931784d38
20tuic017 <20tuic017@skct.edu.in>
2023-07-27 18:12:40 +0530

    array.java added

---
+++ array.java
@@ -0,0 +1,13 @@
+public class array {
+    //Q: store a number
+    int a=10;
+    //Store a person's name
+    String name="Kavya";
+    //Store 5 numbers
+    //syntax
+    //datatype[] variable name=new datatype[size];
+    int [] numbers=new int [5];
+    int [] nums={23,45,3,5,1};
+    int[] nums;//Declration of array,nums is getting defined in the stack
+    nums=new int[5];//Initialisation ; actual memory is created here,here objecys are created in heap memory
+}
